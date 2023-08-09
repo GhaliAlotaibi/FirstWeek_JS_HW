b26716582eadbb538b28fa3575ae3eee46f38897
Ghali <nick_kali@yahoo.com>
2023-08-09 23:04:41 +0300

    Added day4 homeworks

---
+++ Day4/main.js
@@ -0,0 +1,72 @@
+/* التمرين الاول */
+
+// Number to String
+
+const num= 42;
+const numToStr = String(num);
+
+console.log(`${typeof numToStr}:${num}`);
+
+// String to Number
+
+const str= "25";
+const StrToNum = Number(str);
+
+console.log(`${typeof StrToNum}:${str}`);
+
+/*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+
+/* التمرين الثاني */
+
+// String to Number
+
+const str1= "10";
+const StrToNum1 = Number(str1);
+
+console.log(`${typeof StrToNum1}:${str1}`);
+
+// +
+
+console.log(22 + StrToNum1);
+
+// -
+
+console.log(22 - StrToNum1);
+
+/*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+
+/* التمرين الثالث */
+
+// Number to Float
+
+const num1= 15.5;
+const NumToFloat1 = parseFloat(num1);
+
+console.log(`${typeof NumToFloat1}:${num1}`);
+
+// +
+
+console.log(22 * NumToFloat1);
+
+// -
+
+console.log(22 / NumToFloat1);
+
+/*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+
+/* التمرين الرابع */
+
+const num2 = "5";
+const num3 = 3;
+
+console.log(num2 + num3);
+
+/*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+
+/* التمرين الخامس */
+
+const Boo = true;
+const num4 = 1;
+
+console.log(Boo + num4);
+
