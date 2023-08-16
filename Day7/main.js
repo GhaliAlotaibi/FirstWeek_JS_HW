bc94bc5d25edc31e2406a68c9b4bc14ebff08003
Ghali <nick_kali@yahoo.com>
2023-08-16 07:03:35 +0300

    Added Day7 homeworks

---
+++ Day7/main.js
@@ -0,0 +1,31 @@
+// التمرين الاول
+
+let Age = prompt("Please enter your age :")
+
+if (Age >= 18) {
+    alert ("Welcome to Tuwaiq Academy")
+}
+
+if (Age < 18) {
+    alert ("We are sorry to inform you that we cannot accept you into our bootcamp")
+}
+
+else {
+    alert ("The data you enterd is not valid !")
+}
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// التمرين الثاني
+
+// let name_1 = prompt("الرجاء ادخال الاسم :")
+
+// if (name_1 == "احمد") {
+//     alert ("الاسمان متطابقان")
+// }
+
+// else {
+//     alert ("الاسمان غير متطابقان")
+// }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
